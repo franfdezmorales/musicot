dcb2683c21b01b47294a4d4321baa1289938a858
Fran <franfdezmorales@gmail.com>
2022-03-23 12:05:24 +0100

    fixed image without src

--- components/LastUser/index.jsx
+++ components/LastUser/index.jsx
@@ -2,9 +2,12 @@ import styles from './LastUser.module.css'
 import Image from 'next/image'
 
 export function LastUser({name, image}) {
+
+    console.log(image)
+
     return (
         <div className={styles.user}>
-            <Image src={image} width={40} height={40} className={styles.photo} alt={name} />
+            <Image src={image ?? '/noavatar.png'} width={40} height={40} className={styles.photo} alt={name} />
             <span>{name}</span>
         </div>
     )
